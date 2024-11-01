f98774d29c0a57d844edc268b1664c6429077c55
kousra <mdkousarmia71@gmail.com>
2024-11-01 11:20:59 +0600

    card all most doen

--- src/Cpmponents/Book/Book.jsx
+++ src/Cpmponents/Book/Book.jsx
@@ -8,28 +8,39 @@ const Book = ({ book }) => {
             <div className="card-compact rounded-xl bg-base-100 border-2">
                 <figure className='bg-[#F3F3F3] m-6 rounded-xl py-8'>
                     <img
-                        className="max-w-[134px] h-[166px] mx-auto object-cover rounded-md"
+                        className="w-[134px] h-[166px] mx-auto object-cover rounded-md"
                         src={book.image}
                         alt="books.image"
                     />
                 </figure>
                 <div className="card-body">
-                    <div>
-                        <p></p>
-                        <p></p>
+                    <div className='space-x-2'>
+                        {
+                            book.tags.map(tag => <button className="btn btn-xs text-[#23BE0A] font-semibold">{tag}</button>)
+                        }
+
                     </div>
-                    <h2 className="card-title">Shoes!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Buy Now</button>
+                    <h2 className="card-title text-2xl font-bold">{book.bookName}</h2>
+                    <p className='text-lg font-semibold text-gray-500'>By: {book.publisher}</p>
+                    <div className="border-t-2 border-dashed"></div>
+                    <div className="flex flex-row  items-center justify-between w-full">
+                        <p className="text-gray-500 font-bold flex-1">{book.category}</p>
+                        <p className="text-gray-500 font-bold flex-1 text-right">{book.rating}
+                            <div className="rating">
+                                <input type="radio" name="rating-4" className="mask mask-star-2 bg-green-500" />
+                                
+                            </div>
+
+                        </p>
                     </div>
+
+
                 </div>
             </div>
         </div>
 
     );
 };
-
 Book.propTypes = {
 
 };
